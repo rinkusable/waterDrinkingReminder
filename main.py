3f6f29936310101e082d2312f05faea998188498
rinkusable <71535453+rinkusable@users.noreply.github.com>
2021-08-11 16:34:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import time 
+#import time
 from plyer import notification
+if __name__ == "__main__":
+    notification.notify(
+        title = "water drinking reminder",
+        message = "Do your body have suuficient water"
+                  " you have to drink sufficient water for better health."
+                  "its your time to drink water",
+       app_icon = "D:\python projects\drinkingWaterReminder\glass.ico",
+        timeout = 5
+
+    )
